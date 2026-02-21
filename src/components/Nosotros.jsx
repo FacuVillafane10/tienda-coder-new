0fe64e860292a795fa1e36ad2f04ddc6c9971d41
Facundo Villafañe <facuvillafane10@gmail.com>
2026-02-21 14:11:29 -0300

    Cambio en mensaje nosotros

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -4,11 +4,12 @@ export default function Nosotros() {
   return (
     <div className="container mt-5"> 
       <h1 className="mb-4 ">Sobre Nosotros</h1>
-      <p className=''>Somos una empresa dedicada a la venta de productos frescos y de calidad para el hogar. <br />
-        Nuestra misión es ofrecer a nuestros clientes una experiencia de compra fácil, rápida y segura, con productos que cumplen con los más altos estándares de calidad. <br />
-        En Paula Foods, nos esforzamos por brindar un excelente servicio al cliente y una amplia variedad de productos para satisfacer las necesidades de cada hogar. <br />
-        ¡Gracias por elegirnos como tu tienda de confianza!
-        </p>
+      <p className=''>
+        En Paula Foods nos dedicamos a llevarte lo mejor de la comida casera, hecha con amor, directamente a tu casa, empresa o trabajo. <br />
+        Sabemos que lo que necesitas es algo rápido, rico y de calidad. <br />
+        Preparado con ingredientes frescos y de primera. <br />
+        ¡Gracias por elegirnos!
+      </p>
      
         </div> 
   );  
